2c787390f0faffadefbf02e4c9bf353883439f22
Chris Johnston <chjohnston@protonmail.com>
2019-06-16 22:07:36 -0700

    update exposed internals for the unit test project

--- src/Discord.Net.Commands/AssemblyInfo.cs
+++ src/Discord.Net.Commands/AssemblyInfo.cs
@@ -1,3 +1,3 @@
-﻿using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Discord.Net.Tests")]
+[assembly: InternalsVisibleTo("Discord.Net.Tests.Unit")]
